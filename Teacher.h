ff4dcd1d5f5c29e9df50b2dc825a15f22a207bb5
georgiyr101 <129197424+georgiyr101@users.noreply.github.com>
2025-12-08 13:00:42 +0300

    Update Teacher.h

--- Teacher.h
+++ Teacher.h
@@ -8,25 +8,25 @@
 class Teacher : virtual public Human
 {
 protected:
-	String position = "";
-	String specialty = "";
-	String scientificPublications[MAX_PUBLICATIONS];
+	string position = "";
+	string specialty = "";
+	string scientificPublications[MAX_PUBLICATIONS];
 	int publicationsCount = 0;
 public:
 	Teacher() {}
-	Teacher(String firstName, String lastName, int birthYear, String position, String specialty): Human(firstName, lastName, birthYear), position(position), specialty(specialty), publicationsCount(0) {}
+	Teacher(string firstName, string lastName, int birthYear, string position, string specialty): Human(firstName, lastName, birthYear), position(position), specialty(specialty), publicationsCount(0) {}
 	virtual ~Teacher() {};
 	Teacher& operator=(const Teacher& other);
 
-	String getPosition() const;
-	String getSpecialty() const;
+	string getPosition() const;
+	string getSpecialty() const;
 	int getPublicationsCount() const;
-	String getScientificPublication(int index) const;
+	string getScientificPublication(int index) const;
 
-	void setPosition(const String& position);
-	void setSpecialty(const String& specialty);
-	bool addScientificPublication(const String& publication);
-	bool setScientificPublication(int index, const String& publication);
+	void setPosition(const string& position);
+	void setSpecialty(const string& specialty);
+	bool addScientificPublication(const string& publication);
+	bool setScientificPublication(int index, const string& publication);
 
 	virtual char editMenu() const override;
 	virtual void editFields() override;
